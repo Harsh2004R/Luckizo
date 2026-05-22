4dad0147631a80fcfa948e4a9b5a4c3e5a229256
Harsh2004R <harshsharmaktm03@gmail.com>
2026-05-22 19:48:36 +0530

    Player Schema update for production

--- backend/Models/player.model.js
+++ backend/Models/player.model.js
@@ -1,16 +1,67 @@
 import mongoose from "mongoose";
 
-const playerSchema = mongoose.Schema({
-    phone: { type: Number, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, enum: ["user", "admin"], default: "user" },
-    refreshToken: String,
-    profile: { type: String, default: "https://res.cloudinary.com/djbe55v48/image/upload/v1779195557/Fortune-of-wheel/avatar1_fqgqki.png" },
+const withdrawalSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Number,
+      required: true,
+    },
 
-}, {
-    timestamps: true
-});
+    status: {
+      type: String,
+      enum: ["pending", "success", "rejected"],
+      default: "pending",
+    },
+
+    requestedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { _id: false },
+);
+
+const playerSchema = mongoose.Schema(
+  {
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    password: { type: String, required: true, select: false },
+    role: {
+      type: String,
+      enum: ["player", "admin"],
+      default: "player",
+      index: true,
+    },
+    rank: {
+      type: Number,
+      index: true,
+      default: 1,
+    },
+    refreshToken: { type: String, select: false },
+    wallet: { type: Number, default: 0 },
+    withdrawal: [withdrawalSchema],
+    profile: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/djbe55v48/image/upload/v1779195557/Fortune-of-wheel/avatar1_fqgqki.png",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 const PlayerModel = mongoose.model("player", playerSchema);
 export default PlayerModel;

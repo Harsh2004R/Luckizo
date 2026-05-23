49a53e26320ca708ba1afc9c4c4993ee237701be
Harsh2004R <harshsharmaktm03@gmail.com>
2026-05-23 20:42:20 +0530

    Add libraries chakra react-router-dom react-hook-form axios react-icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,47 @@
 // import "./App.css";
-
+import { Container, ScrollArea, Box } from "@chakra-ui/react";
 function App() {
-  return <><text>app</text></>;
+  return (
+    <Container
+      // border={"1px solid tomato"}
+      maxWidth={{ base: "100%", md: "md", lg: "xl" }}
+      minH="100vh"
+      py="1vh"
+      px="0px"
+    >
+      <ScrollArea.Root
+        px={{ base: "5px", md: "7px", lg: "8px" }}
+        // border={"1px solid red"}
+        borderRight={"1px solid #ffffff1e"}
+        borderLeft={"1px solid #ffffff1e"}
+        height="98vh"
+        maxW="100%"
+      >
+        <ScrollArea.Viewport
+          css={{
+            "--scroll-shadow-size": "4rem",
+            maskImage: "linear-gradient(#000, #000)",
+            "&[data-overflow-y]": {
+              maskImage:
+                "linear-gradient(#000,#000,transparent 0,#000 var(--scroll-shadow-size),#000 calc(100% - var(--scroll-shadow-size)),transparent)",
+              "&[data-at-top]": {
+                maskImage:
+                  "linear-gradient(180deg,#000 calc(100% - var(--scroll-shadow-size)),transparent)",
+              },
+              "&[data-at-bottom]": {
+                maskImage:
+                  "linear-gradient(0deg,#000 calc(100% - var(--scroll-shadow-size)),transparent)",
+              },
+            },
+          }}
+        >
+          <ScrollArea.Content spaceY="4">
+            {/* Scroll Container ..... */}
+          </ScrollArea.Content>
+        </ScrollArea.Viewport>
+      </ScrollArea.Root>
+    </Container>
+  );
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 // import "./index.css";
+import { Provider as ChakraProvider } from "../src/components/ui/provider.jsx";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
+);

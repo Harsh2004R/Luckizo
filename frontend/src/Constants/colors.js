3dedd92ed516e0d5b4cb385119d83cc3ab0e9a90
Harsh2004R <harshsharmaktm03@gmail.com>
2026-05-31 12:50:05 +0530

    Navbar Theme Added

--- frontend/src/Constants/colors.js
+++ frontend/src/Constants/colors.js
@@ -1,9 +1,100 @@
 const colors = {
   input_lable: "#ddddddf4",
   form_border: "1px solid #7070703e",
-  caption_text_color:"grey",
-  caption_warning:"yellow.500",
-  header_color : "#dadada",
+  caption_text_color: "grey",
+  caption_warning: "yellow.500",
+  header_color: "#dadada",
+
+  // navbar colors ... variations
+  // default
+  // Theme 1- Riot Games / Valorant
+  navbar_theme: {
+    background: "rgba(18,18,18,.95)",
+    border: "rgba(255,255,255,.05)",
+
+    active: "#FF4655",
+    activeHover: "#FF6B76",
+
+    inactive: "#8A8A8A",
+
+    activeBg: "rgba(255,70,85,.12)",
+    activeBorder: "rgba(255,70,85,.25)",
+
+    glow: "rgba(255,70,85,.15)",
+    shadow: "rgba(0,0,0,.7)",
+  },
+
+  // Theme 2 — Emerald Neon (My Favorite)
+  // navbar_theme: {
+  //   background: "rgba(13,17,23,0.95)",
+  //   border: "rgba(255,255,255,0.06)",
+
+  //   active: "#00E5A8",
+  //   activeHover: "#2AF5BC",
+
+  //   inactive: "#7D8590",
+
+  //   activeBg: "rgba(0,229,168,.12)",
+  //   activeBorder: "rgba(0,229,168,.25)",
+
+  //   glow: "rgba(0,229,168,.15)",
+  //   shadow: "rgba(0,0,0,.6)",
+  // },
+
+  // Theme 3 — Cyber Blue
+  //   navbar_theme:{
+  //   background: "rgba(13,17,23,.95)",
+  //   border: "rgba(255,255,255,.06)",
+
+  //   active: "#3B82F6",
+  //   activeHover: "#60A5FA",
+
+  //   inactive: "#7D8590",
+
+  //   activeBg: "rgba(59,130,246,.12)",
+  //   activeBorder: "rgba(59,130,246,.25)",
+
+  //   glow: "rgba(59,130,246,.15)",
+  //   shadow: "rgba(0,0,0,.6)",
+  // },
+
+  // Theme 4 — Premium Gold
+  //   navbar_theme:{
+  //   background: "rgba(15,15,18,.95)",
+  //   border: "rgba(255,255,255,.05)",
+
+  //   active: "#F7C948",
+  //   activeHover: "#FFD76A",
+
+  //   inactive: "#8B8B8B",
+
+  //   activeBg: "rgba(247,201,72,.12)",
+  //   activeBorder: "rgba(247,201,72,.25)",
+
+  //   glow: "rgba(247,201,72,.15)",
+  //   shadow: "rgba(0,0,0,.65)",
+  // },
+
+  // Theme 5 — Cyber Purple
+
+  // navbar_theme:{
+  //   background: "rgba(16,18,27,.95)",
+  //   border: "rgba(255,255,255,.05)",
+
+  //   active: "#8B5CF6",
+  //   activeHover: "#A78BFA",
+
+  //   inactive: "#7D8590",
+
+  //   activeBg: "rgba(139,92,246,.12)",
+  //   activeBorder: "rgba(139,92,246,.25)",
+
+  //   glow: "rgba(139,92,246,.15)",
+  //   shadow: "rgba(0,0,0,.6)",
+  // },
+
+
+  
 };
 
 export default colors;
